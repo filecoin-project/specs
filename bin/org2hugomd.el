3147367e37a9a5665c2036a8cce7d34199071e4b
jbenet <juan@benet.ai>
2019-09-16 03:16:29 -0700

    script to install orent deps for user

--- bin/org2hugomd.el
+++ bin/org2hugomd.el
@@ -1,15 +1,7 @@
 #!/usr/bin/env -S emacs -Q --script
 
-;; Sandbox
-(setq
- user-emacs-directory (concat (file-name-directory load-file-name) ".emacs/")
- package-user-dir (concat user-emacs-directory "elpa/")
- use-package-always-ensure t
- inhibit-message t) ; if there are errors, remove this.
- ; debug-on-error t) ; if there are errors, add this.
-
 ;; load info
-(load (concat (file-name-directory load-file-name) "emacs-deps.el"))
+(load (concat (file-name-directory load-file-name) "emacs-init-build.el"))
 
 ;; process input
 (with-temp-buffer
