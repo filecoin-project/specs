07a4677b4d42f846276c5e7e7dca23ead66b0b12
jbenet <juan@benet.ai>
2019-09-16 04:24:38 -0700

    fixing publish to gh-pages

--- bin/publish-to-gh-pages.sh
+++ bin/publish-to-gh-pages.sh
@@ -1,42 +1,50 @@
 #!/bin/sh
 
+source "$(dirname $0)/lib.sh"
+
+src=build/website
+bdir=build/gh-pages
+
 #------- WIP FLAG
 echo "Hold up, this isn't ready yet."
-echo "if you need it, ping @jbenet to fix it"
-exit 1;
-#-------
+echo "if you need it, ping @jbenet to fix it\n"
 
-if [[ $(git status -s) ]]
-then
-    echo "error: the working directory is dirty."
-    echo "Please commit any pending changes and test, before publishing."
-    exit 1;
+echo "WARNING: this will publish to github pages."
+# get confirmation from user
+if [ "$1" != "-y" ]; then
+  get_user_confirmation
 fi
 
-echo "Deleting old publication"
-rm -rf public
-mkdir public
-git worktree prune
-rm -rf .git/worktrees/public/
+#-------
 
-echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public origin/gh-pages
+[[ $(git status -s -uno) ]] && die "the working directory is dirty.\n\
+Please commit any pending changes and test, before publishing."
+
+[ -d "$src" ] || die "$src not found. did you run: make website ?"
+
+echo "Prepare worktree"
+# rm -rf public
+# mkdir public
+# git worktree prune
+# rm -rf .git/worktrees/public/
+
+echo "Checking out gh-pages branch into build/gh-pages"
+git worktree add -B gh-pages "$bdir" origin/gh-pages
 
 echo "Removing existing files"
-rm -rf public/*
+rm -rf "$bdir/*"
 
-echo "Generating PDF"
-name=$(bin/build-pdf.sh)
-echo $name
-cp "pdf-build/$name" "static/$name"
-msg="You can also download the full spec in [PDF format](.\/$name)."
-sed -i "" "s/<\!\-\- REPLACE_ME_PDF_LINK \-\->/$msg/" INTRO.md
-
-echo "Generating site"
-make website
+# echo "Generating PDF"
+# name=$(bin/build-pdf.sh)
+# echo $name
+# cp "pdf-build/$name" "static/$name"
+# msg="You can also download the full spec in [PDF format](.\/$name)."
+# sed -i "" "s/<\!\-\- REPLACE_ME_PDF_LINK \-\->/$msg/" INTRO.md
 
 echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+shash=$(git rev-parse --short HEAD)
+cd "$bdir" && git add --all && git commit -m "Publishing $shash ($0)"
 
 echo "Publishing to github"
+die "cannot publish to github. this is WIP, so only dry runs."
 git push origin gh-pages
