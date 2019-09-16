c0bc74f17ac4d7d05c139400e89d629e625ccfc0
jbenet <juan@benet.ai>
2019-09-16 05:08:43 -0700

    updated publish-to-gh-pages.sh script

--- bin/publish-to-gh-pages.sh
+++ bin/publish-to-gh-pages.sh
@@ -2,37 +2,36 @@
 
 source "$(dirname $0)/lib.sh"
 
-src=build/website
-bdir=build/gh-pages
+website=build/website
+ghpages=build/gh-pages
 
-#------- WIP FLAG
-echo "Hold up, this isn't ready yet."
-echo "if you need it, ping @jbenet to fix it\n"
-
-echo "WARNING: this will publish to github pages."
+## -- warnings
+# echo "WARNING: this will publish to github pages."
 # get confirmation from user
-if [ "$1" != "-y" ]; then
-  get_user_confirmation
-fi
+# if [ "$1" != "-y" ]; then
+#  get_user_confirmation
+# fi
 
-#-------
+## -- checks
+must_run_from_spec_root
+# [[ $(git status -s -uno) ]] && die "the working directory is dirty.\n\
+# Please commit any pending changes and test, before publishing."
+[ -d "$website" ] || die "$website not found. did you run: make website ?"
 
-[[ $(git status -s -uno) ]] && die "the working directory is dirty.\n\
-Please commit any pending changes and test, before publishing."
 
-[ -d "$src" ] || die "$src not found. did you run: make website ?"
+## -- work tree setup
+echo "Setting up worktree in build/gh-pages"
+git fetch origin gh-pages
+git worktree remove gh-pages
+rm -rf "$ghpages"
+git worktree add -B gh-pages "$ghpages" origin/gh-pages
 
-echo "Prepare worktree"
-# rm -rf public
-# mkdir public
-# git worktree prune
-# rm -rf .git/worktrees/public/
-
-echo "Checking out gh-pages branch into build/gh-pages"
-git worktree add -B gh-pages "$bdir" origin/gh-pages
-
+## -- updating website content
 echo "Removing existing files"
-rm -rf "$bdir/*"
+dotgit=$(cat "$ghpages/.git") # preserve .git
+rm -rf "$ghpages"
+cp -r "$website" "$ghpages"
+echo "$dotgit" >"$ghpages/.git" # paste .git
 
 # echo "Generating PDF"
 # name=$(bin/build-pdf.sh)
@@ -43,8 +42,9 @@ rm -rf "$bdir/*"
 
 echo "Updating gh-pages branch"
 shash=$(git rev-parse --short HEAD)
-cd "$bdir" && git add --all && git commit -m "Publishing $shash ($0)"
+cd "$ghpages" && git add --all && git commit -m "Publishing $shash ($0)"
 
+## -- publishing
 echo "Publishing to github"
 die "cannot publish to github. this is WIP, so only dry runs."
-git push origin gh-pages
+# git push origin gh-pages
