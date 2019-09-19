28fa758b8a17fef7004e353cb4e7ff2595dbdd5c
porcuquine <porcuquine@users.noreply.github.com>
2019-09-18 23:17:13 -0700

    Generate Orient HTML report.

--- bin/build-spec-orient.sh
+++ bin/build-spec-orient.sh
@@ -3,6 +3,8 @@ input_dir=src/orient
 
 orient/bin/orient solve --system=$input_dir/filecoin.orient --in=$input_dir/params.json | jq > $output_dir/solved-parameters.json
 
+orient/bin/orient report --system=$input_dir/filecoin.orient --in=$input_dir/params.json  > $output_dir/filecoin-report.html
+
 orient/bin/orient dump --system=$input_dir/filecoin.orient | jq > $output_dir/filecoin.json
 
 orient/bin/orient graph --system=$input_dir/filecoin.orient $input_dir/params.json | dot -Tsvg > $output_dir/filecoin.svg
