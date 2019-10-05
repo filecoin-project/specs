9b0a083341364d435a061af9cf669ece20f7cb13
jbenet <juan@benet.ai>
2019-10-04 22:23:30 -0600

    mining/sector_index builds

--- src/systems/filecoin_mining/sector_index/sector_index_subsystem.go
+++ src/systems/filecoin_mining/sector_index/sector_index_subsystem.go
@@ -1,51 +1,24 @@
 package sector_index
 
-func (sis *SectorIndexerSubsystem_I) AddDealToSector(deal StorageDeal) AddDealToSectorResponse {
-	addPieceResponse := sis.SectorBuilder.AddPiece(deal.PiecePath)
-	pip := sis.StorageProofs.GetPieceInclusionProof(deal.PieceRef)
+import (
+	blockchain "github.com/filecoin-project/specs/systems/filecoin_blockchain"
+	// piece "github.com/filecoin-project/specs/systems/filecoin_files/piece"
+	mkt "github.com/filecoin-project/specs/systems/filecoin_markets"
+	sector "github.com/filecoin-project/specs/systems/filecoin_mining/sector"
+)
 
-	return AddDealToSectorResponse{
-		sectorID: addPieceResponse.SectorInfo.ID,
-		pip:      pip,
-	}
-
-	// sectorbuilder := SectorBuilders[sectorConfig];
-	// piecePath := SectorStore.WritePiece(piece);
-	// response := sectorBuilder.addPiece(PiecePath);
-	// MaybeSeal(response.StagedSector, response.BytesRemaining);
+func (sis *SectorIndexerSubsystem_I) AddNewDeal(deal mkt.StorageDeal) error {
+	return sis.Builder().StageDeal(deal)
 }
 
-// func (sis *SectorIndexerSubsystem_I) Seal(stagedSector StagedSector) {
-// 	sealedPath := SectorStore.AllocateSealedSector(stagedSector.SectorSize);
-// 	response := SectorSealer.Seal(stagedSector, sealedPath, ProverId);
-// 	SectorStore.RegisterMetadata(
-// 	SectorMetadata {
-// 	  response.CommR,
-// 		response.PersistentAux,
-// 		response.PartialMerkleTreePath,
-// 	});
+// func (sis *SectorIndexerSubsystem_I) OnNewTipset(chain blockchain.Chain, epoch blockchain.Epoch) {
+// 	panic("TODO")
 // }
 
-func (sis *SectorIndexerSubsystem_I) selectSectorBuilderByDeal(deal StorageDeal) SectorBuilder {
+func (sis *SectorIndexerSubsystem_I) SectorsExpiredAtEpoch(epoch blockchain.Epoch) []sector.SectorID {
 	panic("TODO")
 }
 
-func (sis *SectorIndexerSubsystem_I) indexSectorByDealExpiration(sectorID SectorID, deal StorageDeal) {
-	panic("TODO")
-}
-
-func (sis *SectorIndexerSubsystem_I) getPieceInclusionProof(pieceRef CID) PieceInclusionProof {
-	panic("TODO")
-}
-
-func (sis *SectorIndexerSubsystem_I) OnNewTipset(chain Chain, epoch Epoch) {
-	panic("TODO")
-}
-
-func (sis *SectorIndexerSubsystem_I) lookupSectorByExpiry(currentEpoch Epoch) []SectorID {
-	panic("TODO")
-}
-
-func (sis *SectorIndexerSubsystem_I) purgeSectorWithNoLiveDeals(sectorIDs []SectorID) {
+func (sis *SectorIndexerSubsystem_I) removeSectors(sectorIDs []sector.SectorID) {
 	panic("TODO")
 }
