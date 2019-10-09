284150c05cd5a827f94e8c5824fd060f289da1d5
nicola <nicola@users.noreply.github.com>
2019-10-09 18:21:00 +0900

    fixed sector import in storage_miner_actor.go

--- src/systems/filecoin_mining/storage_mining/storage_miner_actor.go
+++ src/systems/filecoin_mining/storage_mining/storage_miner_actor.go
@@ -1,7 +1,8 @@
 package storage_mining
 
 import base_mining "github.com/filecoin-project/specs/systems/filecoin_mining"
+import sector "github.com/filecoin-project/specs/systems/filecoin_mining/sector"
 
-func (sm *StorageMinerActor) SubmitPoSt(postProof base_mining.PoStProof, sectorStateSets SectorStateSets) {
+func (sm *StorageMinerActor) SubmitPoSt(postProof base_mining.PoStProof, sectorStateSets sector.SectorStateSets) {
 
 }
