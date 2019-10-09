2f9721c16b12ecc9dbd80753591366d6edb24358
nicola <nicola@users.noreply.github.com>
2019-10-09 18:20:03 +0900

    fixing storage_miner_actor.go import issue

--- src/systems/filecoin_mining/storage_mining/storage_miner_actor.go
+++ src/systems/filecoin_mining/storage_mining/storage_miner_actor.go
@@ -1,6 +1,6 @@
 package storage_mining
 
-// import base_ "github.com/filecoin-project/specs/systems/filecoin_mining"
+import base_mining "github.com/filecoin-project/specs/systems/filecoin_mining"
 
 func (sm *StorageMinerActor) SubmitPoSt(postProof base_mining.PoStProof, sectorStateSets SectorStateSets) {
 
