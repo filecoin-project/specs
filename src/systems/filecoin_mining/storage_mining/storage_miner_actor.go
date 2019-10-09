d18baa388f7719eb706f944031cce95c3f83f82b
nicola <nicola@users.noreply.github.com>
2019-10-09 18:21:49 +0900

    storage miner actor use implementation instead of interface

--- src/systems/filecoin_mining/storage_mining/storage_miner_actor.go
+++ src/systems/filecoin_mining/storage_mining/storage_miner_actor.go
@@ -3,6 +3,6 @@ package storage_mining
 import base_mining "github.com/filecoin-project/specs/systems/filecoin_mining"
 import sector "github.com/filecoin-project/specs/systems/filecoin_mining/sector"
 
-func (sm *StorageMinerActor) SubmitPoSt(postProof base_mining.PoStProof, sectorStateSets sector.SectorStateSets) {
+func (sm *StorageMinerActor_I) SubmitPoSt(postProof base_mining.PoStProof, sectorStateSets sector.SectorStateSets) {
 
 }
