bc738c32b4861305497d78fb7ec9e4731236c607
nicola <nicola@users.noreply.github.com>
2019-10-10 20:33:56 +0900

    no more errors

--- src/systems/filecoin_mining/storage_mining/storage_mining_subsystem.go
+++ src/systems/filecoin_mining/storage_mining/storage_mining_subsystem.go
@@ -35,39 +35,41 @@ func (sms *StorageMiningSubsystem_I) CommitSectorError() base_markets.StorageDea
 }
 
 func (sms *StorageMiningSubsystem_I) OnNewTipset(chain blockchain.Chain, epoch blockchain.Epoch, tipset blockchain.Tipset) {
-	sms.CurrentChain = chain
-	sms.CurrentEpoch = epoch
-	sms.CurrentTipset = tipset
+	panic("TODO")
 }
 
 func (sms *StorageMiningSubsystem_I) OnNewRound(newTipset blockchain.Tipset) base_blockchain.ElectionArtifacts {
-	ea := sms.Consensus().GetElectionArtifacts(sms.CurrentChain, sms.CurrentEpoch)
-	EP := sms.DrawElectionProof(ea.TK(), sms.workerPrivateKey)
-
 	panic("TODO: fix this below")
+
+	// TODO this below has been commented due to incomplete implementation
+	// ea := sms.Consensus().GetElectionArtifacts(sms.CurrentChain, sms.CurrentEpoch)
+	// EP := sms.DrawElectionProof(ea.TK(), sms.workerPrivateKey)
 	// if newTipset {
 	// 	T0 := GenerateNextTicket(ea.T1, workerPrivateKey)
 	// } else {
 	// 	T1 := GenerateNextTicket(T0, workerPrivateKey)
 	// }
 
-	if sms.Consensus().TryLeaderElection(EP) {
-		// TODO: move this into SPC or Blockchain
-		// SMS should probably not have ability to call BlockProducer directly.
-		sms.BlockProducer().GenerateBlock(EP, ea.T0(), sms.CurrentTipset(), workerKey)
-	} else {
-		// TODO when not elected
-	}
+	// if sms.Consensus().TryLeaderElection(EP) {
+	// 	// TODO: move this into SPC or Blockchain
+	// 	// SMS should probably not have ability to call BlockProducer directly.
+	// 	sms.BlockProducer().GenerateBlock(EP, ea.T1(), sms.CurrentTipset(), workerKey)
+	// } else {
+	// 	// TODO when not elected
+	// }
+
+	// return ea
 }
 
 func (sms *StorageMiningSubsystem_I) DrawElectionProof(tk base_blockchain.Ticket, workerKey filcrypto.PrivKey) base_blockchain.ElectionProof {
-	return generateElectionProof(tk, workerKey)
+	// return generateElectionProof(tk, workerKey)
+	panic("TODO")
 }
 
 func (sms *StorageMiningSubsystem_I) GenerateNextTicket(t1 base_blockchain.Ticket, workerKey filcrypto.PrivKey) base_blockchain.Ticket {
 	panic("TODO")
 }
 
-func (sp *StorageProvider) NotifyStorageDealStaged(storageDealNotification StorageDealStagedNotification) {
+func (sp *StorageProvider_I) NotifyStorageDealStaged(storageDealNotification StorageDealStagedNotification) {
 	panic("TODO")
 }
