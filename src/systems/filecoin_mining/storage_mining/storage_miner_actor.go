ebe43287b8e00eb5fdfecfb151521f4f2d351315
nicola <nicola@users.noreply.github.com>
2019-10-13 15:49:55 +0900

    added sealseed related structs

--- src/systems/filecoin_mining/storage_mining/storage_miner_actor.go
+++ src/systems/filecoin_mining/storage_mining/storage_miner_actor.go
@@ -8,7 +8,16 @@ func (sma *StorageMinerActor_I) SubmitPoSt(postProof base_mining.PoStProof, sect
 	panic("TODO")
 }
 
+// TODO: currently deals must be posted on chain via sma.PublishStorageDeals
+// as an optimization, in the future CommitSector could contain a list of
+// deals that are not published yet.
 func (sma *StorageMinerActor_I) CommitSector(onChainInfo sealing.OnChainSealVerifyInfo) {
+	// TODO: sma.verifySeal(sectorID SectorID, comm sealing.OnChainSealVerifyInfo, proof SealProof)
+
+	// TODO: you don't need to store the proof and the randomseed in the state tree
+	// just verify it and drop it, just SealCommitments{CommD, CommR, DealIDs}
+	// TODO: @porcuquine verifies
+
 	currSector, found := sma.Sectors()[onChainInfo.SectorNumber()]
 	if !found {
 		sma.Sectors()[onChainInfo.SectorNumber()] = []sealing.OnChainSealVerifyInfo{onChainInfo}
@@ -17,5 +26,5 @@ func (sma *StorageMinerActor_I) CommitSector(onChainInfo sealing.OnChainSealVeri
 		sma.Sectors()[onChainInfo.SectorNumber()] = newSector
 	}
 
-	// TODO broadcast message on chain
+	// TODO write state change
 }
