1cf451c8d13ecf7c26f88cfd07c120681e6af332
nicola <nicola@users.noreply.github.com>
2019-10-14 18:09:08 +0900

    rebase

--- src/systems/filecoin_mining/storage_mining/storage_miner_actor.go
+++ src/systems/filecoin_mining/storage_mining/storage_miner_actor.go
@@ -27,7 +27,9 @@ func (sm *StorageMinerActor_I) OnFaultBeingSpotted() {
 // - Expired Sectors (settle deals..)
 // TODO: remove nextFaultSet from here, instead, inherith faults from previous
 // proving period
-func (sm *StorageMinerActor_I) SubmitPoSt(postSubmission poster.PoStSubmission, nextFaultSet sector.FaultSet) {
+
+// TODO: if something is faulty, move it to committed instead
+func (sm *StorageMinerActor_I) SubmitPoSt(postSubmission poster.PoStSubmission) {
 	// Verify correct PoSt Submission:
 	// 1. A proof must be submitted after the postRandomness for this proving
 	// period is on chain
@@ -50,40 +52,42 @@ func (sm *StorageMinerActor_I) SubmitPoSt(postSubmission poster.PoStSubmission, 
 	// The proof is verified, proceed to sector state transitions:
 	// var powerUpdate uint
 
-	// State change: Committed -> Active
+	// State change: Unproven -> Active
 	// Note: this must be the first state transition check.
-	// {
-	// powerUpdate = powerUpdate + len(sm.CommittedSectors()) * sm.info().sectorSize()
-	// sm.CommittedSectors = []
-	// }
+	{
+		powerUpdate = powerUpdate + len(sm.UnprovenSectors_()) * sm.info().sectorSize()
+		sm.UnprovenSectors_ = []
+	}
 
 	// 1. State change: Active -> Faulty
 	// Note: this must happend after Committed->Active, in order to account for
 	// sectors that have been committed in this proving period which also happen
 	// to be faulty.
+	// Note: if the miner has not recovered the faults, the are re-declared
+	// automatically.
 	{
 		// Handle faulty sectors
 		// sm.NextFaultSet_ = all zeros
-		// sm.DeclareFault(nextFaultSet)
+		// sm.DeclareFault(sm.nextFaultSet)
 	}
 
 	// 2. State change: Faulty -> Active
 	{
-		// Handle Recovered faults:
-		// If a sector is not in sm.NextFaultSet at this point, it means that it
-		// was just proven in this proving period.
-		// However, if this had a counter in sm.Faults, then it means that it was
-		// faulty in the previous proving period and then recovered.
-		// In that case, reset the counter and resume power.
+	// Handle Recovered faults:
+	// If a sector is not in sm.NextFaultSet at this point, it means that it
+	// was just proven in this proving period.
+	// However, if this had a counter in sm.Faults, then it means that it was
+	// faulty in the previous proving period and then recovered.
+	// In that case, reset the counter and resume power.
 
-		// resumedSectorsCount := 0
-		// for previouslyFaulty := range keys(sm.Faults) {
-		//   if (previouslyFaulty not in sm.NextFaultSet()) {
-		//     delete(sm.Faults, previouslyFaulty)
-		//     resumedSectorsCount = resumedSectorsCount + 1
-		//   }
-		// }
-		// powerUpdate = powerUpdate + resumedSectorsCount * sm.info().sectorSize()
+	// resumedSectorsCount := 0
+	// for previouslyFaulty := range keys(sm.Faults) {
+	//   if (previouslyFaulty not in sm.NextFaultSet() and previouslyFaulty in sm.UnprovenSectors_) {
+	//     delete(sm.Faults, previouslyFaulty)
+	//     resumedSectorsCount = resumedSectorsCount + 1
+	//   }
+	// }
+	// powerUpdate = powerUpdate + resumedSectorsCount * sm.info().sectorSize()
 	}
 
 	// Pay all the Active sectors
@@ -98,7 +102,7 @@ func (sm *StorageMinerActor_I) SubmitPoSt(postSubmission poster.PoStSubmission, 
 		// }
 	}
 
-	// 3. State change: Active -> Expired
+	// 3. State change: Active -> Deleted (because they are expired)
 	// Note: this must happen as last state transition check to ensure that
 	// payments and faults have been accounted for correctly.
 	{
@@ -131,35 +135,48 @@ func (sm *StorageMinerActor_I) SubmitPoSt(postSubmission poster.PoStSubmission, 
 	}
 }
 
-func (sm *StorageMinerActor_I) DeclareFault(newFaults sector.FaultSet) {
+func (sm *StorageMinerActor_I) DecoverFaults(newFaults sector.FaultSet) {
+	// add them to the UnprovenSectors_
+	// update FaultSet
+}
+
+func (sm *StorageMinerActor_I) DeclareFaults(newFaults sector.FaultSet) {
 	// Handle Fault
 
 	// TODO: the faults that are declared after post challenge,
 	// are faults for the next proving period
 
+
+	// TODO: below is a bit complicated, it should be simplified.
+
 	// Update Fault Set
 
-	// TODO: batch into a single message
+	// var lostPower uint
 	// for sectorNumber := range newFaultSet {
 	//   // Avoid penalizing the miner multiple times in the same proving period
 	//   if !(sector is in sm.NextFaultSet()) {
+	//     // it is a new fault:
+	//     lostPower = lostPower + sm.info().sectorSize()
 	//     if (sectorNumber not in sm.Faults) sm.Faults[sectorNumber] = 0
 	//     sm.Faults[sectorNumber] = sm.Faults[sectorNumber] + 1
-
 	//     if (sm.Faults[sectorNumber] > MAX_CONSECUTIVE_FAULTS_ALLOWED) {
 	//       Sector is lost, delete it, slash storage deal collateral and all pledge
 	//       SendMessage(sma.SlashAllStorageDealCollateral(dealIDs)
 	//       SendMessage(spa.SlashAllPledgeCollateral(sectorNumber)
 	//       delete(sm.Sectors(), expiredSectorNumber)
 	//     } else {
-	//       dealIDs := sm.Sectors()[sectorNumber].DealIDs()
+	//       append(sm.UnprovenSectors_, sectorNumber)
 	//     }
 	//   }
 	// }
 
-	// TODO: delete power for faulty sectors
-	// TODO: check if we want to penalize some collateral for losing some files
+	// Delete Power
+	// SendMessage(sma.UpdatePower(- lostPower))
+
+	// Store updated fault set
 	// sm.nextFaultSet.applyDiff(newFaultSet)
+
+	// TODO: check if we want to penalize some collateral for losing some files
 
 	panic("TODO")
 }
