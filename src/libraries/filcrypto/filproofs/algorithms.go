ee21292b89d048594320be127720e977ed6fed57
porcuquine <porcuquine@users.noreply.github.com>
2019-10-17 18:30:46 -0700

    Initial cut at RepHash.

--- src/libraries/filcrypto/filproofs/algorithms.go
+++ src/libraries/filcrypto/filproofs/algorithms.go
@@ -256,11 +256,55 @@ func WideRepCompress_Blake2sHash(data []byte) Blake2sHash {
 
 ////////////////////////////////////////////////////////////////////////////////
 
+func DigestSize_T() int {
+	panic("Unspecialized")
+}
+
+func DigestSize_PedersenHash() int {
+	return 32
+}
+
+func DigestSize_Blake2sHash() int {
+	return 32
+}
+
+////////////////////////////////////////////////////////////////////////////////
 /// Binary Merkle-tree generation
+
 // RepHash<T>
 func RepHash_T(data []byte) (util.T, file.Path) {
 	// Plan: define this in terms of RepCompress_T, then copy-paste changes into T-specific specializations, for now.
-	return util.T{}, file.Path("") // FIXME
+
+	// Nodes are always the digest size so data cannot be compressed to digest for storage.
+	nodeSize := DigestSize_T()
+
+	// TODO: Fail if len(dat) is not a power of 2 and an even multiple of the node size.
+
+	rows := [][]byte{data}
+
+	for row := []byte{}; len(row) > nodeSize; {
+		for i := 0; i < len(data); i += 2 * nodeSize {
+			left := data[i : i+nodeSize]
+			right := data[i+nodeSize : i+2*nodeSize]
+			hashed := RepCompress_T(left, right)
+
+			row = append(row, asBytes(hashed)...)
+		}
+		rows = append(rows, row)
+	}
+
+	// Last row is the root
+	root := rows[len(rows)-1]
+
+	if len(root) != nodeSize {
+		panic("math failed us")
+	}
+
+	var file // TODO: dump tree to file.
+	// NOTE: merkle tree file layout is illustrative, not prescriptive.
+	
+	// TODO: Check above more carefully. It's just an untested sketch for the moment.
+	return fromBytes_T(root), file 
 }
 
 // RepHash<PedersenHash>
@@ -308,4 +352,15 @@ func littleEndianBytesFromBigInt(z *big.Int, size int) []byte {
 	reverse(bytes)
 
 	return bytes
+}
+
+func asBytes(t util.T) []byte {
+	panic("Unimplemented for T")
+
+	return []byte{}
+}
+
+func fromBytes_T(_ interface{}) util.T {
+	panic("Unimplemented for T")
+	return util.T{}
 }
