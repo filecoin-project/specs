3448d3cb2b10be3882b126b900455d572f490d4e
zixuanzh <zixuanzh@seas.upenn.edu>
2019-10-17 21:11:13 -0700

    build power table

--- src/systems/filecoin_blockchain/storage_power_consensus/storage_power_actor.go
+++ src/systems/filecoin_blockchain/storage_power_consensus/storage_power_actor.go
@@ -1,10 +1,8 @@
 package storage_power_consensus
 
 import (
-	filcrypto "github.com/filecoin-project/specs/libraries/filcrypto"
 	block "github.com/filecoin-project/specs/systems/filecoin_blockchain/struct/block"
 	deal "github.com/filecoin-project/specs/systems/filecoin_markets/deal"
-	sector "github.com/filecoin-project/specs/systems/filecoin_mining/sector"
 	addr "github.com/filecoin-project/specs/systems/filecoin_vm/actor/address"
 	util "github.com/filecoin-project/specs/util"
 )
@@ -83,66 +81,4 @@ func (spa *StoragePowerActor_I) Surprise(ticket block.Ticket) []addr.Address {
 	}
 
 	return surprisedMiners
-}
-
-// Power Table
-
-func (pt *PowerTable_I) RegisterMiner(addr addr.Address, pk filcrypto.PubKey, sectorSize sector.SectorSize) {
-	panic("")
-	// newMiner := &StorageMiner_I{
-	// 	MinerAddress_:        addr,
-	// 	MinerStoragePower_:   0,
-	// 	MinerSuspendedPower_: 0,
-	// 	MinerPK_:             pk,
-	// 	MinerSectorSize_:     sectorSize,
-	// }
-	// pt.miners[&addr] = *newMiner
-}
-
-func (pt *PowerTable_I) GetMinerPower(addr addr.Address) block.StoragePower {
-	panic("")
-	// return pt.miners[addr].MinerStoragePower
-}
-
-func (pt *PowerTable_I) GetTotalPower() block.StoragePower {
-	panic("")
-	// totalPower := 0
-	// for _, miner := range pt.miners {
-	// 	totalPower += miner.MinerStoragePower
-	// }
-	// return totalPower
-}
-
-func (pt *PowerTable_I) GetMinerPublicKey(addr addr.Address) filcrypto.PubKey {
-	panic("")
-	// return pt.miners[addr].MinerPK
-}
-
-func (pt *PowerTable_I) IncrementPower(addr addr.Address, numSectors util.UVarint) {
-	panic("")
-	// pt.miners[addr].MinerStoragePower += numSectors * pt.miners[addr].minerSectorSize
-}
-
-// must be atomic
-func (pt *PowerTable_I) SuspendPower(addr addr.Address, numSectors util.UVarint) {
-	panic("")
-	// pt.miners[addr].MinerStoragePower -= numSectors * pt.miners[addr].minerSectorSize
-	// pt.miners[addr].MinerSuspendedPower += numSectors * pt.miners[addr].minerSectorSize
-}
-
-// must be atomic
-func (pt *PowerTable_I) UnsuspendPower(addr addr.Address, numSectors util.UVarint) {
-	panic("")
-	// pt.miners[addr].MinerSuspendedPower -= numSectors * pt.miners[addr].minerSectorSize
-	// pt.miners[addr].MinerStoragePower += numSectors * pt.miners[addr].minerSectorSize
-}
-
-func (pt *PowerTable_I) RemovePower(addr addr.Address, numSectors util.UVarint) {
-	panic("")
-	// pt.miners[addr].MinerSuspendedPower -= numSectors * pt.miners[addr].minerSectorSize
-}
-
-func (pt *PowerTable_I) RemoveAllPower(addr addr.Address, numSectors util.UVarint) {
-	panic("")
-	// pt.miners[addr].MinerStoragePower = 0
 }
