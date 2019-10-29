98468a4cdc457ecfe1c61dfddee48d28f90dae2b
porcuquine <porcuquine@users.noreply.github.com>
2019-10-28 22:16:20 -0700

    Use correct script when solving a single orient system.

--- bin/solve-orient.sh
+++ bin/solve-orient.sh
@@ -6,4 +6,6 @@ output_file=$3
 
 mkdir -p $output_file && rmdir $output_file # Dirty hack to ensure output_file directory exists.
 
+echo $output_file
+
 orient/bin/orient solve --system=$system_file --in=$input_file | jq > $output_file
