990f897df95969e3b3df77a822eec0dfe8bf2adc
porcuquine <porcuquine@users.noreply.github.com>
2019-10-28 21:11:38 -0700

    Fix all-orient target for previous changes.

--- bin/build-spec-orient.sh
+++ bin/build-spec-orient.sh
@@ -6,12 +6,12 @@ mkdir -p $output_dir
 mkdir -p $diagram_dir
 
 orient/bin/orient solve --system=$input_dir/filecoin.orient --in=$input_dir/snark-table.json | jq > $output_dir/snark-table.json
-orient/bin/orient solve --system=$input_dir/filecoin.orient --in=$input_dir/params.json | jq > $output_dir/solved-parameters.json
+orient/bin/orient solve --system=$input_dir/filecoin.orient --in=$input_dir/filecoin.json | jq > $output_dir/solved-parameters.json
 orient/bin/orient solve --system=$input_dir/filecoin.orient --in=$input_dir/multi-params.json | jq > $output_dir/multi-solved-parameters.json
 orient/bin/orient solve --system=$input_dir/fast-porep.orient --in=$input_dir/fast-porep.json | jq > $output_dir/fast-porep.json
 
-orient/bin/orient report --system=$input_dir/filecoin.orient --in=$input_dir/params.json  > $output_dir/filecoin-report.html
+orient/bin/orient report --system=$input_dir/filecoin.orient --in=$input_dir/filecoin.json  > $output_dir/filecoin-report.html
 
 orient/bin/orient dump --system=$input_dir/filecoin.orient | jq > $output_dir/filecoin.json
 
-orient/bin/orient graph --system=$input_dir/filecoin.orient --in=$input_dir/params.json > $diagram_dir/filecoin.dot
+orient/bin/orient graph --system=$input_dir/filecoin.orient --in=$input_dir/filecoin.json > $diagram_dir/filecoin.dot
