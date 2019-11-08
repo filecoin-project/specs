e7fa46ceca059b6b7d49ce820c97ed372199f8df
zixuanzh <zixuanzh@users.noreply.github.com>
2019-11-08 15:39:08 -0500

    add CompactDealSet

--- src/systems/filecoin_mining/sector/sectorset.go
+++ src/systems/filecoin_mining/sector/sectorset.go
@@ -1,5 +1,7 @@
 package sector
 
+import deal "github.com/filecoin-project/specs/systems/filecoin_markets/deal"
+
 // TODO
 func (css *CompactSectorSet) SectorsOn() []SectorNumber {
 	var sectorNo []SectorNumber
@@ -34,4 +36,41 @@ func (css *CompactSectorSet) DeepCopy() CompactSectorSet {
 func (css *CompactSectorSet) Extend(css2 CompactSectorSet) CompactSectorSet {
 	var newCompactSectorSet CompactSectorSet
 	return newCompactSectorSet
+}
+
+// TODO
+func (css *CompactSectorSet) Contain(sectorNo SectorNumber) bool {
+	return false
+}
+
+/////////////////////////////////////////////////////////////////////////
+
+// TODO
+func (css *CompactDealSet) DealsOn() []deal.DealID {
+	var dealId []deal.DealID
+	return dealId
+}
+
+// TODO
+func (css *CompactDealSet) DealsOff() []deal.DealID {
+	var dealId []deal.DealID
+	return dealId
+}
+
+// TODO
+func (css *CompactDealSet) Add(dealId deal.DealID) CompactDealSet {
+	var newCompactDealSet CompactDealSet
+	return newCompactDealSet
+}
+
+// TODO
+func (css *CompactDealSet) Remove(dealId deal.DealID) CompactDealSet {
+	var newCompactDealSet CompactDealSet
+	return newCompactDealSet
+}
+
+// TODO
+func (css *CompactDealSet) Extend(css2 CompactDealSet) CompactDealSet {
+	var newCompactDealSet CompactDealSet
+	return newCompactDealSet
 }
