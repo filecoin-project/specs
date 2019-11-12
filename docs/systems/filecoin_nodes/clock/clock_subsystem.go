7517d6f85a622a32ec763116d21554d318c5100f
jbenet <juan@benet.ai>
2019-11-12 03:11:20 -0800

    Publishing c1cdef5f (bin/publish-to-gh-pages.sh)

--- docs/systems/filecoin_nodes/clock/clock_subsystem.go
+++ docs/systems/filecoin_nodes/clock/clock_subsystem.go
@@ -2,34 +2,50 @@ package clock
 
 import "time"
 
-func (_ *Clock_I) NowUTC() Time {
-	return Time(time.Now().Unix())
+// UTCMaxDrift is how large the allowable drift is in Filecoin's use of UTC time.
+var UTCMaxDrift = time.Second
+
+// UTCSyncPeriod notes how often to sync the UTC clock with an authoritative
+// source, such as NTP, or a very precise hardware clock.
+var UTCSyncPeriod = time.Hour
+
+// EpochDuration is a constant that represents the UTC time duration
+// of a blockchain epoch.
+var EpochDuration = time.Second * 15
+
+// ISOFormat is the ISO timestamp format we use, in Go time package notation.
+var ISOFormat = "2006-01-02T15:04:05.999999999Z"
+
+func (_ *UTCClock_I) NowUTC() Time {
+	return Time(time.Now().Format(ISOFormat))
 }
 
-func (_ *Clock_I) NowUTCNano() Time {
-	return Time(time.Now().UnixNano())
+func (_ *UTCClock_I) NowUTCUnix() UnixTime {
+	return UnixTime(time.Now().Unix())
 }
 
-// given some starting epoch and time since, what epoch should this be?
-func (self *Clock_I) epochAfterTime(start ChainEpoch, t Time) ChainEpoch {
-	panic("")
-	// return start + t/epochDuration
+func (_ *UTCClock_I) NowUTCUnixNano() UnixTimeNano {
+	return UnixTimeNano(time.Now().UnixNano())
 }
 
-func (self *Clock_I) EpochAfterGenesis() ChainEpoch {
-	panic("")
-	// return self.epochAfterTime(0 + self.NowUTCNano() - self.genesisTime)
-}
+// EpochAtTime returns the ChainEpoch corresponding to t.
+// It first subtracts GenesisTime, then divides by EpochDuration
+// and returns the resulting number of epochs.
+func (c *ChainEpochClock_I) EpochAtTime(t Time) ChainEpoch {
+	g1 := c.GenesisTime()
+	g2, err := time.Parse(ISOFormat, string(g1))
+	if err != nil {
+		// an implementation should probably not panic here
+		// this is for simplicity of the spec
+		panic(err)
+	}
 
-func (self *Clock_I) currentEpochStart() Time {
-	panic("")
-	// return EpochAfterGenesis() * self.epochDuration + genesisTime
-}
+	t2, err := time.Parse(ISOFormat, string(t))
+	if err != nil {
+		panic(err)
+	}
 
-func (self *Clock_I) CurrentEpochState() ChainEpoch {
-	panic("")
-	// if self.NowUTCNano()-self.currentEpochStart() > self.cutoffAt_ {
-	// 	return ChainEpochState.PastCutoff
-	// }
-	// return ChainEpochState.Active
+	difference := t2.Sub(g2)
+	epochs := difference / EpochDuration
+	return ChainEpoch(epochs)
 }
