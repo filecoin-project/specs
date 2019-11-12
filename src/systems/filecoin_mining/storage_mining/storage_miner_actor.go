d84bf9f4b2a6de10ffbd67f6723be917d01bcfdf
porcuquine <porcuquine@users.noreply.github.com>
2019-11-12 09:44:26 -0800

    Fix rebase error: use new name (SendCatchingErrors).

--- src/systems/filecoin_mining/storage_mining/storage_miner_actor.go
+++ src/systems/filecoin_mining/storage_mining/storage_miner_actor.go
@@ -580,7 +580,7 @@ func (st *StorageMinerActorState_I) _isSealVerificationCorrect(rt Runtime, onCha
 	params := make([]actor.MethodParam, 1+len(dealIDs))
 	// TODO: serialize method param as {sectorSize,  DealIDs...}.
 
-	receipt := rt.SendAllowingErrors(&msg.InvocInput_I{
+	receipt := rt.SendCatchingErrors(&msg.InvocInput_I{
 		To_:     addr.StorageMarketActorAddr,
 		Method_: storage_market.MethodGetUnsealedCIDForDealIDs,
 		Params_: params,
