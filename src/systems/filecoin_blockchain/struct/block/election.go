9bc6d740a8dbd2701647e4e828d1b6bd2cb9dd6f
zixuanzh <zixuanzh@users.noreply.github.com>
2019-11-19 15:16:58 -0500

    fix compile error

--- src/systems/filecoin_blockchain/struct/block/election.go
+++ src/systems/filecoin_blockchain/struct/block/election.go
@@ -10,17 +10,13 @@ func (tix *Ticket_I) ValidateSyntax() bool {
 	return tix.VRFResult_.ValidateSyntax()
 }
 
-// TODO: add SHA256 to filcrypto
-// TODO: import SHA256 from filcrypto
-var SHA256 = func([]byte) []byte { return nil }
-
-func (tix *Ticket_I) Output() bytes {
-	return SHA256(tix.VRFResult_.Output_)
+func (tix *Ticket_I) Output() util.Bytes {
+	return SHA256(tix.VRFResult().Output())
 }
 
 func (tix *Ticket_I) Verify(input util.Bytes, pk filcrypto.VRFPublicKey) bool {
 	//	return tix.VRFResult_.Verify(append([]byte(filcrypto.TicketTag), input...), pk)
-	return tix.VRFResult_.Verify(input, pk) && tix.Output_ == SHA256(tix.VRFResult_.Output_)
+	return tix.VRFResult_.Verify(input, pk) && sliceEqual(tix.Output(), SHA256(tix.VRFResult().Output()))
 }
 
 func (tix *Ticket_I) DrawRandomness(minerAddr addr.Address, epoch ChainEpoch) util.Bytes {
