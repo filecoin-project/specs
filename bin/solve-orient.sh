78eaf7d3c1f0f9c506a532264337c0eaaf803be2
nikkolasg <nikkolasg@gmail.com>
2019-11-26 18:48:32 +0000

    using docker orient

--- bin/solve-orient.sh
+++ bin/solve-orient.sh
@@ -4,8 +4,17 @@ system_file=$1
 input_file=$2
 output_file=$3
 
+if [[ -z "${ORIENT_DCALC}" ]]; then
+    orient_bin=orient/bin/orient
+else
+    orient_bin=orient/bin/dcalc
+    system_file=/orientd/$system_file
+    input_file=/orientd/$input_file
+    output_file=/orientd/$output_file
+fi;
+
 mkdir -p $output_file && rmdir $output_file # Dirty hack to ensure output_file directory exists.
 
 echo $output_file
 
-orient/bin/orient solve --system=$system_file --in=$input_file | jq > $output_file
+$orient_bin solve --system=$system_file --in=$input_file | jq > $output_file
