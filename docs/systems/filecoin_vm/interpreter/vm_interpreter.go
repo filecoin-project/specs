557b2b3a8e859d064ef5b91af5d55bb56a056234
Henri S <sternhenri@users.noreply.github.com>
2019-11-28 17:41:57 -0500

    Publishing 23f2582a (bin/publish-to-gh-pages.sh)

--- docs/systems/filecoin_vm/interpreter/vm_interpreter.go
+++ docs/systems/filecoin_vm/interpreter/vm_interpreter.go
@@ -1,42 +1,70 @@
 package interpreter
 
-import msg "github.com/filecoin-project/specs/systems/filecoin_vm/message"
-import addr "github.com/filecoin-project/specs/systems/filecoin_vm/actor/address"
-import actor "github.com/filecoin-project/specs/systems/filecoin_vm/actor"
-import exitcode "github.com/filecoin-project/specs/systems/filecoin_vm/runtime/exitcode"
-import st "github.com/filecoin-project/specs/systems/filecoin_vm/state_tree"
-import vmr "github.com/filecoin-project/specs/systems/filecoin_vm/runtime"
-import gascost "github.com/filecoin-project/specs/systems/filecoin_vm/runtime/gascost"
-import util "github.com/filecoin-project/specs/util"
+import (
+	ipld "github.com/filecoin-project/specs/libraries/ipld"
+	actor "github.com/filecoin-project/specs/systems/filecoin_vm/actor"
+	addr "github.com/filecoin-project/specs/systems/filecoin_vm/actor/address"
+	msg "github.com/filecoin-project/specs/systems/filecoin_vm/message"
+	vmr "github.com/filecoin-project/specs/systems/filecoin_vm/runtime"
+	exitcode "github.com/filecoin-project/specs/systems/filecoin_vm/runtime/exitcode"
+	gascost "github.com/filecoin-project/specs/systems/filecoin_vm/runtime/gascost"
+	st "github.com/filecoin-project/specs/systems/filecoin_vm/state_tree"
+	util "github.com/filecoin-project/specs/util"
+)
+
+type Bytes = util.Bytes
 
 var TODO = util.TODO
 
-func (vmi *VMInterpreter_I) ApplyMessageBatch(inTree st.StateTree, msgs []MessageRef) (outTree st.StateTree, ret []msg.MessageReceipt) {
-	compTree := inTree
-	for _, m := range msgs {
-		oT, r := vmi.ApplyMessage(compTree, m.Message(), m.Miner())
-		compTree = oT        // assign the current tree. (this call always succeeds)
-		ret = append(ret, r) // add message receipt
-	}
-	return compTree, ret
-}
+const (
+	// TODO: reduce these when expected gas costs are known
+	sumbitElectionPostGasLimit = 1e18
+	payBlockRewardGasLimit     = 1e1
+	cronTickGasLimit           = 1e18
+)
 
-func _applyError(errCode exitcode.SystemErrorCode) msg.MessageReceipt {
-	// TODO: should this gasUsed value be zero?
-	// If nonzero, there is not guaranteed to be a nonzero gas balance from which to deduct it.
-	gasUsed := gascost.ApplyMessageFail
-	TODO()
-	return msg.MessageReceipt_MakeSystemError(errCode, gasUsed)
-}
+// Applies all the message in a tipset, along with implicit block- and tipset-specific state
+// transitions.
+func (vmi *VMInterpreter_I) ApplyTipSetMessages(inTree st.StateTree, msgs TipSetMessages) (outTree st.StateTree, receipts []msg.MessageReceipt) {
+	outTree = inTree
+	seenMsgs := make(map[ipld.CID]struct{}) // CIDs of messages already seen once.
+	var r msg.MessageReceipt
+	for _, blk := range msgs.Blocks() {
+		// Pay block reward.
+		reward := _makeBlockRewardMessage(outTree, blk.Miner())
+		outTree, r = vmi.ApplyMessage(outTree, reward, blk.Miner())
+		if r.ExitCode() != exitcode.OK() {
+			panic("block reward failed")
+		}
 
-func _withTransferFundsAssert(tree st.StateTree, from addr.Address, to addr.Address, amount actor.TokenAmount) st.StateTree {
-	// TODO: assert amount nonnegative
-	retTree, err := tree.Impl().WithFundsTransfer(from, to, amount)
-	if err != nil {
-		panic("Interpreter error: insufficient funds (or transfer error) despite checks")
-	} else {
-		return retTree
+		// Process block miner's Election PoSt.
+		epost := _makeElectionPoStMessage(outTree, blk.Miner(), msgs.Epoch(), blk.PoStProof())
+		outTree, r = vmi.ApplyMessage(outTree, epost, blk.Miner())
+		if r.ExitCode() != exitcode.OK() {
+			panic("election post failed")
+		}
+
+		// Process messages from the block.
+		for _, m := range blk.Messages() {
+			_, found := seenMsgs[_msgCID(m)]
+			if found {
+				continue
+			}
+			outTree, r = vmi.ApplyMessage(outTree, m, blk.Miner())
+			receipts = append(receipts, r)
+			seenMsgs[_msgCID(m)] = struct{}{}
+		}
 	}
+
+	// Invoke cron tick, attributing it to the miner of the first block.
+	cronSender := msgs.Blocks()[0].Miner()
+	cron := _makeCronTickMessage(outTree, cronSender)
+	outTree, r = vmi.ApplyMessage(outTree, cron, cronSender)
+	if r.ExitCode() != exitcode.OK() {
+		panic("cron tick failed")
+	}
+
+	return
 }
 
 func (vmi *VMInterpreter_I) ApplyMessage(inTree st.StateTree, message msg.UnsignedMessage, minerAddr addr.Address) (
@@ -54,7 +82,7 @@ func (vmi *VMInterpreter_I) ApplyMessage(inTree st.StateTree, message msg.Unsign
 	}
 
 	// make sure fromActor has enough money to run the max invocation
-	maxGasCost := gasToFIL(message.GasLimit(), message.GasPrice())
+	maxGasCost := _gasToFIL(message.GasLimit(), message.GasPrice())
 	totalCost := message.Value() + actor.TokenAmount(maxGasCost)
 	if fromActor.Balance() < totalCost {
 		return inTree, _applyError(exitcode.InsufficientFunds)
@@ -102,7 +130,7 @@ func (vmi *VMInterpreter_I) ApplyMessage(inTree st.StateTree, message msg.Unsign
 			outTree,
 			message.From(),
 			addr.BurntFundsActorAddr,
-			gasToFIL(sendRet.GasUsed(), message.GasPrice()),
+			_gasToFIL(sendRet.GasUsed(), message.GasPrice()),
 		)
 	} else {
 		// success -- refund unused gas
@@ -113,7 +141,7 @@ func (vmi *VMInterpreter_I) ApplyMessage(inTree st.StateTree, message msg.Unsign
 			outTree,
 			addr.BurntFundsActorAddr,
 			message.From(),
-			gasToFIL(refundGas, message.GasPrice()),
+			_gasToFIL(refundGas, message.GasPrice()),
 		)
 	}
 
@@ -128,12 +156,90 @@ func (vmi *VMInterpreter_I) ApplyMessage(inTree st.StateTree, message msg.Unsign
 		outTree,
 		addr.BurntFundsActorAddr,
 		minerAddr, // TODO: may not exist
-		gasToFIL(sendRet.GasUsed(), message.GasPrice()),
+		_gasToFIL(sendRet.GasUsed(), message.GasPrice()),
 	)
 
 	return outTree, sendRet
 }
 
-func gasToFIL(gas msg.GasAmount, price msg.GasPrice) actor.TokenAmount {
+func _applyError(errCode exitcode.SystemErrorCode) msg.MessageReceipt {
+	// TODO: should this gasUsed value be zero?
+	// If nonzero, there is not guaranteed to be a nonzero gas balance from which to deduct it.
+	gasUsed := gascost.ApplyMessageFail
+	TODO()
+	return msg.MessageReceipt_MakeSystemError(errCode, gasUsed)
+}
+
+func _withTransferFundsAssert(tree st.StateTree, from addr.Address, to addr.Address, amount actor.TokenAmount) st.StateTree {
+	// TODO: assert amount nonnegative
+	retTree, err := tree.Impl().WithFundsTransfer(from, to, amount)
+	if err != nil {
+		panic("Interpreter error: insufficient funds (or transfer error) despite checks")
+	} else {
+		return retTree
+	}
+}
+
+func _gasToFIL(gas msg.GasAmount, price msg.GasPrice) actor.TokenAmount {
 	return actor.TokenAmount(util.UVarint(gas) * util.UVarint(price))
+}
+
+// Builds a message for paying block reward from the treasury account to a miner owner.
+func _makeBlockRewardMessage(state st.StateTree, minerActorAddr addr.Address) msg.UnsignedMessage {
+	// networkTreasuryActor := loadActor(NetworkTreasuryActorAddress)
+	// minerActor := loadActorSubstate(minerActorAddr)
+	// minerWorker := loadActor(minerActor.Info.Worker)
+	//return &UnsignedMessage_I{
+	//	From_:       minerActor.Info.Worker,
+	//	To_:         NetworkTreasuryActorAddress,
+	//	Method_:     NetworkTreasury.PayBlockReward,
+	//	Params_:     serialize([minerActorAddr]),
+	//	CallSeqNum_: minerWorker.CallSeqNum,
+	//	Value_:      0,
+	//	GasPrice_:   0,
+	//	GasLimit_:   payBlockRewardGasLimit,
+	//}
+	panic("TODO: implement when network treasury actor implemented")
+}
+
+// Builds a message for submitting ElectionPost on behalf of a miner actor.
+func _makeElectionPoStMessage(state st.StateTree, minerActorAddr addr.Address, epoch UInt64, postProof Bytes) msg.UnsignedMessage {
+	// minerActor := loadActorSubstate(minerActorAddr)
+	// minerWorker := loadActor(minerActor.Info.Worker)
+	//return &UnsignedMessage_I{
+	//	From_:       minerActor.Info.Worker,
+	//	To_:         minerActorAddr,
+	//	Method_:     StorageMinerActor.SubmitElectionPoSt,
+	//	Params_:     serialize([PoStSubmission{postProof, epoch}]),
+	//	CallSeqNum_: minerWorker.CallSeqNum,
+	//	Value_:      0,
+	//	GasPrice_:   0,
+	//	GasLimit_:   sumbitElectionPostGasLimit,
+	//}
+	panic("TODO: implement when necessary dependencies are importable")
+}
+
+// Builds a message for invoking the cron actor tick.
+func _makeCronTickMessage(state st.StateTree, minerActorAddr addr.Address) msg.UnsignedMessage {
+	// minerActor := loadActorSubstate(minerActorAddr)
+	// minerWorker := loadActor(minerActor.Info.Worker)
+	//return &UnsignedMessage_I{
+	//	From_:       minerActor.Info.Worker,
+	//	To_:         CronActorAddress,
+	//	Method_:     CronActor.EpochTick,
+	//	Params_:     nil,
+	//	CallSeqNum_: minerWorker.CallSeqNum,
+	//	Value_:      0,
+	//	GasPrice_:   0,
+	//	GasLimit_:   cronTickGasLimit,
+	//}
+	panic("TODO: implement when necessary dependencies are importable")
+}
+
+func _msgCID(msg msg.UnsignedMessage) ipld.CID {
+	panic("TODO")
+}
+
+func _encodeParams(p []interface{}) Bytes {
+	panic("TODO")
 }
