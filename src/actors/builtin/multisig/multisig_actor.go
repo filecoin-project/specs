ed1c28c14cafdd98caa060ab52cc3993b6c6bfaf
frrist <forrest@protocol.ai>
2020-01-15 18:49:34 -0800

    refactor: create multisig actor state file

--- src/actors/builtin/multisig/multisig_actor.go
+++ src/actors/builtin/multisig/multisig_actor.go
@@ -18,7 +18,7 @@ var IMPL_FINISH = autil.IMPL_FINISH
 var IMPL_TODO = autil.IMPL_TODO
 var TODO = autil.TODO
 
-type TxnID int
+type TxnID int64
 
 type MultiSigTransaction struct {
 	Proposer   addr.Address
@@ -46,46 +46,6 @@ func MultiSigTransactionHAMT_Empty() MultiSigTransactionHAMT {
 func MultiSigApprovalSetHAMT_Empty() MultiSigApprovalSetHAMT {
 	IMPL_FINISH()
 	panic("")
-}
-
-type MultiSigActorState struct {
-	// Linear unlock
-	InitialBalance abi.TokenAmount
-	StartEpoch     abi.ChainEpoch
-	UnlockDuration abi.ChainEpoch
-
-	AuthorizedParties     autil.ActorIDSetHAMT
-	NumApprovalsThreshold int
-	NextTxnID             TxnID
-	PendingTxns           MultiSigTransactionHAMT
-	PendingApprovals      MultiSigApprovalSetHAMT
-}
-
-func (st *MultiSigActorState) AmountLocked(elapsedEpoch abi.ChainEpoch) abi.TokenAmount {
-	if elapsedEpoch >= st.UnlockDuration {
-		return abi.TokenAmount(0)
-	}
-
-	TODO() // BigInt
-	lockedProportion := (st.UnlockDuration - elapsedEpoch) / st.UnlockDuration
-	return abi.TokenAmount(uint64(st.InitialBalance) * uint64(lockedProportion))
-}
-
-// return true if MultiSig maintains required locked balance after spending the amount
-func (st *MultiSigActorState) _hasAvailable(currBalance abi.TokenAmount, amountToSpend abi.TokenAmount, currEpoch abi.ChainEpoch) bool {
-	if amountToSpend < 0 || currBalance < amountToSpend {
-		return false
-	}
-
-	if currBalance-amountToSpend < st.AmountLocked(currEpoch-st.StartEpoch) {
-		return false
-	}
-
-	return true
-}
-
-func (st *MultiSigActorState) CID() cid.Cid {
-	panic("TODO")
 }
 
 type MultiSigActor struct{}
