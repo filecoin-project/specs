d2e0aeecfb6e801432a0f4e869cebf4c9de31bcb
nicola <nicola@users.noreply.github.com>
2020-05-11 21:42:26 +0200

    adding sequential hashing

--- src/orient/viz/nse.notebook.js
+++ src/orient/viz/nse.notebook.js
@@ -30,6 +30,7 @@ combos = makeQuery([
     rig_cost: 4750,
     rig_storage_lifetime_years: 2,
     rig_cost_storage_tb: 15,
+    rig_hashing_sequential_throughput_gb_s: 2.5,
   })
   .add({
     mtree_hash_name: 'poseidon',
