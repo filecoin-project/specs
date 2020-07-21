cc962d8f336fba8eb2c8c2e1b4358b446f2df1ad
Oli Evans <oli@tableflip.io>
2020-07-21 09:32:41 +0100

    wip: reduce flash of incorrect theme
    License: MIT
    Signed-off-by: Oli Evans <oli@tableflip.io>

--- assets/dark-mode/dark-mode.js
+++ assets/dark-mode/dark-mode.js
@@ -1,41 +1,52 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-  // Swap the active css based on the users color-scheme preference.
-  const btn = document.querySelector('.dark-mode-toggle')
+// iife to avoid polluting the global.
+(function () {
+  // Run me as soon as possible after the the css links are in the dom.
+  // This assumes this js file is added to the page after the css links.
   const lightMode = document.getElementById('light-mode-link')
   const darkMode = document.getElementById('dark-mode-link')
-
-  const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)') 
-  const previousChoice = localStorage.getItem('theme')
-  let theme = previousChoice || (prefersDarkScheme.matches ? 'dark' : 'light')
-
-  // Listen for a click on the button 
-  btn.addEventListener('click', function () {
-    // toggle the theme name
-    theme = (theme === 'light' ? 'dark' : 'light')
-    
-    if (theme === 'dark') {
-      enableDarkMode()
-    } else {
-      enableLightMode()
-    }
-    localStorage.setItem('theme', theme)
-  })
+  const btn = document.querySelector('.dark-mode-toggle')
+  const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)').matches 
+  let theme = prefersDarkScheme ? 'dark' : 'light'
 
   function enableLightMode () {
     lightMode.disabled = false
     darkMode.disabled = true
-    btn.setAttribute('aria-pressed', false)
+    btn.setAttribute('aria-pressed', theme === 'dark')
   }
 
   function enableDarkMode () {
     darkMode.disabled = false
     lightMode.disabled = true
-    btn.setAttribute('aria-pressed', true)
+    btn.setAttribute('aria-pressed', theme === 'dark')
+  }
+  
+  // enable dark theme optimistically on OS with dark theme enabled to reduce flashing of white theme.
+  if (prefersDarkScheme) {
+    enableDarkMode()
   }
 
-  // Light is default, so enable dark if user previously chose it.
+  // wait for localstorage...
+  const previousChoice = localStorage.getItem('theme')
+  theme = previousChoice || theme
+  
+  // Light is default, so enable dark if user previously chose it but their OS pref is light.
   if (theme === 'dark') {
     enableDarkMode()
   }
 
-});
+  // set up the toggle once the DOM is ready.
+  document.addEventListener("DOMContentLoaded", function(event) {
+    
+    btn.addEventListener('click', function () {
+      theme = (theme === 'light' ? 'dark' : 'light')
+      if (theme === 'dark') {
+        enableDarkMode()
+      } else {
+        enableLightMode()
+      }
+      localStorage.setItem('theme', theme)
+    })
+    // init the button state to match the currently selected theme.
+    btn.setAttribute('aria-pressed', theme === 'dark')
+  });  
+})()
