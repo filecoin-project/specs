a09013b0937cd8cd8138456a6724922b55a3ec1a
Oli Evans <oli@tableflip.io>
2020-09-01 11:37:48 +0100

    chore: remove unused js
    License: MIT
    Signed-off-by: Oli Evans <oli@tableflip.io>

--- assets/js/process-html.js
+++
@@ -1,7 +0,0 @@
-import { buildTocModel } from './content-model'
-import { buildToc } from './toc.js'
-import { buildDashboard } from './dashboard-spec'
-
-const model = buildTocModel('.markdown')
-buildToc({tocSelector:'.toc', model })
-buildDashboard('#dashboard-container', model)
